6b74df3183e87a1f38531112f3c9ee395071ec87
Luca Stasio <lucastasio@MBP-di-Luca.itresources.it>
2019-07-29 11:02:53 +0200

    configurable Configure() setter

--- pkg/machinery/machinery.go
+++ pkg/machinery/machinery.go
@@ -116,7 +116,7 @@ func (m *Machinery) startGears() {
 }
 
 // GetGear returns a Gear instance pointer
-func (m *Machinery) GetGear(name string) interface{} {
+func (m *Machinery) GetGear(name string) *Gear {
 	gear := m.gears[name]
 	return &gear
 }
